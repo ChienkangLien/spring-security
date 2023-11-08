23d2782715d360d2dc558b89ff696608567f6dd0
Howard Lien <chienkang1114@gmail.com>
2023-11-09 00:13:16 +0800

    security_thymeleaf init

---
+++ security_thymeleaf/src/main/java/org/tutorial/Security_thymeleafApplication.java
@@ -0,0 +1,13 @@
+package org.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Security_thymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Security_thymeleafApplication.class, args);
+	}
+
+}
